169260eba9e40f128efbfa001f7661d40913a874
sismijoseph <sismijoseph1997@gmail.com>
2023-09-08 16:21:24 +0530

    calender andnotes changes

--- Screens/MoodTracker2/Components/Calender.js
+++ Screens/MoodTracker2/Components/Calender.js
@@ -2,15 +2,17 @@ import React, { useState } from 'react';
 import { View, StyleSheet, Image } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 import { FONTS } from '../../../Constants/Constants';
+import moment from 'moment';
 
 const Calenders = (props) => {
-    const [CurrentDay, setCurrentDay] = useState(new Date())
+    const [CurrentDay, setCurrentDay] = useState(moment(new Date()).format('YYYY-MM-DD'))
 
     return (
 
         <View style={styles.CardView}>
             <Calendar
                 onDayPress={day => {
+                   console.log(day.dateString)
                     props.setSelected(day.dateString);
                 }}
                 renderArrow={(direction) => {
@@ -20,12 +22,12 @@ const Calenders = (props) => {
                         <Image source={require('../assets/Icon.jpg')} />
                     );
                 }}
-              
                 theme={{
 
                     backgroundColor: '#ffffff',
                     calendarBackground: '#ffffff',
                     textSectionTitleColor: '#b6c1cd',
+
                     textSectionTitleDisabledColor: '#d9e1e8',
                     selectedDayBackgroundColor: '#00adf5',
                     selectedDayTextColor: '#ffffff',
@@ -76,9 +78,19 @@ const Calenders = (props) => {
                 }}
 
                 firstDay={1}
-
+                
                 markedDates={{
-                    [props?.selected]:{selected: true, marked: true,selectedDotColor:'#151F6D',selectedColor:'#F2F4F7'}
+                    [CurrentDay]:{selected: true, marked: true,selectedDotColor:'#151F6D',selectedColor:'#F2F4F7'},
+                    [props?.selected]:{selected: true,selectedDotColor:'#151F6D',selectedColor:'#F2F4F7'},
+                    ['2023-09-01']:{selected: true,selectedColor:'#F2F4F7'},
+                    ['2023-09-02']:{selected: true,selectedColor:'#F2F4F7'},
+                    ['2023-09-03']:{selected: true,selectedColor:'#F2F4F7'},
+                    ['2023-09-04']:{selected: true,selectedColor:'#F2F4F7'},
+                    ['2023-09-05']:{selected: true,selectedColor:'#F2F4F7'},
+                    ['2023-09-06']:{selected: true,selectedColor:'#F2F4F7'},
+                    ['2023-09-07']:{selected: true,selectedColor:'#FEEDBF'}
+
+
                 }}
             
 
